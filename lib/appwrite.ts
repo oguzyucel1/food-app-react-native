b86ea7516acff54e16483b0cdd3eaa8ec1f6eea4
Oğuz Yücel <oguzyucell.oy@gmail.com>
2025-07-12 14:21:38 +0300

    data fetching and rendering implemented

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { CreateUserParams, SignInParams } from "@/type";
+import { CreateUserParams, GetMenuParams, SignInParams } from "@/type";
 import {
   Account,
   Avatars,
@@ -95,6 +95,35 @@ export const getCurrentUser = async () => {
 
     if (!currentUser) throw Error;
     return currentUser.documents[0];
+  } catch (e) {
+    throw new Error(e as string);
+  }
+};
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+  try {
+    const queries: string[] = [];
+    if (category) queries.push(Query.equal("category", category));
+    if (query) queries.push(Query.search("name", query));
+
+    const menus = await database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.menuCollectionId,
+      queries
+    );
+    return menus.documents;
+  } catch (e) {
+    throw new Error(e as string);
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const categories = await database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.categoriesCollectionId
+    );
+    return categories.documents;
   } catch (e) {
     throw new Error(e as string);
   }
